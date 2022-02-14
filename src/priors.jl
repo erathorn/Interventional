891e6bec04eb6c47c91d1137860bd44ba2e94f6d
erathorn <wahle.joh@gmail.com>
2022-02-14 15:33:31 +0100

    Missing Prior Function

--- src/priors.jl
+++ src/priors.jl
@@ -1,26 +1,39 @@
 
-    function HammingPrior!(
-        prior::Matrix{<:Real},
-        priorGraph::Matrix{<:Real},
-        P::Int,
-        maxin::Int,
-    )::Nothing
-        @inbounds for (i, par) in enumerate(powerset(1:P, 0, maxin)), p = 1:P
-            prior[p, i] = sum(x -> (x in par) != priorGraph[x, p], 1:P)
-        end
+
+function set_normalised_prior!(
+    normalisedPrior::Matrix{<:Real},
+    parentCount::Vector{<:Real},
+    maxindegree::Int,
+)::Nothing
+    normalisedPrior .= -log(1 + maxindegree)
+    @inbounds for j = 1:maxindegree
+        wh = findall(parentCount .== j)
+        normalisedPrior[:, wh] .-= log(length(wh))
+    end
+end
+
+function HammingPrior!(
+    prior::Matrix{<:Real},
+    priorGraph::Matrix{<:Real},
+    P::Int,
+    maxin::Int,
+)::Nothing
+    @inbounds for (i, par) in enumerate(powerset(1:P, 0, maxin)), p = 1:P
+        prior[p, i] = sum(x -> (x in par) != priorGraph[x, p], 1:P)
+    end
+end
+
+
+
+
+function MukherjeePrior!(
+    prior::Matrix{<:Real},
+    priorGraph::Matrix{<:Real},
+    P::Int,
+    maxin::Int,
+)::Nothing
+    @inbounds for (i, par) in enumerate(powerset(1:P, 0, maxin)), p = 1:P
+        prior[p, i] = sum(x -> ((x in par) && (priorGraph[x, p] == 0)), 1:P)
     end
 
-
-
-
-    function MukherjeePrior!(
-        prior::Matrix{<:Real},
-        priorGraph::Matrix{<:Real},
-        P::Int,
-        maxin::Int,
-    )::Nothing
-        @inbounds for (i, par) in enumerate(powerset(1:P, 0, maxin)), p = 1:P
-            prior[p, i] = sum(x -> ((x in par) && (priorGraph[x, p] == 0)), 1:P)
-        end
-
-    end
+end
